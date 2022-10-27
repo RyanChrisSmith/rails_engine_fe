cbf688dc417134b0cff07018b9aaba9ec12a07ba
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-27 09:22:06 -0600

    added bootstrap

--- spec/factories/items.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :item do
-    name { Faker::Fantasy::Tolkien.character }
-    description { Faker::Fantasy::Tolkien.poem }
-    unit_price { Faker::Number.decimal(l_digits: 2) }
-    merchant
-  end
-end
